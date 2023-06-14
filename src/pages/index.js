9b2059e6679963317abb1bc3c5ef13284549dd86
Octa96mz <octa96mz@gmail.com>
2023-06-14 19:04:27 -0300

    cambio de titulo

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>React Shop</title>
+        <title>Next Store</title>
       </Head>
       <ProductList />
     </>
